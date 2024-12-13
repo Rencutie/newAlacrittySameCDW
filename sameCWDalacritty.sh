b8513b05994041f9451fd5cf1b8a874c65db0d51
Marwan <155101441+Rencutie@users.noreply.github.com>
2024-12-13 15:20:22 +0100

    Add files via upload

---
+++ sameCWDalacritty.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# git the PID of the currently active window
+# if you are not using hyprland, you can change this 
+# line for it to match your needs
+ACTIVE_PID=$(hyprctl activewindow | awk '/pid:/ {print $2}')
+# If no active window, start in home directory
+if [[ -z "$ACTIVE_PID" ]]; then
+    alacritty
+    exit 0
+fi
+
+# (The CDW is in the shell process, not the terminal itself) 
+# Find the child process of the active window
+CHILD_PID=$(pgrep -P "$ACTIVE_PID" -o)
+if [[ -z "$CHILD_PID" ]]; then
+    alacritty
+    exit 0
+fi
+
+# Get the name of the process
+PROCESS_NAME=$(ps -p "$CHILD_PID" -o comm=)
+
+# Check if the process is a terminal shell or terminal emulator
+if [[ "$PROCESS_NAME" = alacritty ]]; then
+    # Get the current working directory of the shell process
+    SHELL_CWD=$(pwdx "$CHILD_PID" | cut -d ' ' -f2-)
+    if [[ -n "$SHELL_CWD" && -d "$SHELL_CWD" ]]; then
+        alacritty --working-directory "$SHELL_CWD"
+        exit 0
+    fi
+fi
+
+# If no terminal is detected, starts in home
+alacritty
+exit 0
+
